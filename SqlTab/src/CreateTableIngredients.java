30ae5cddfdb56dafb7b6839474af2a63b43ad3f7
Riccardo <jack7riki@gmail.com>
2023-10-06 13:58:57 +0200

    correzione tabella

--- SqlTab/src/CreateTableIngredients.java
+++ SqlTab/src/CreateTableIngredients.java
@@ -1,24 +1,22 @@
 import java.sql.*;
+
 public class CreateTableIngredients {
     public static void main(String[] args) {
-        String url= "jdbc:mysql://localhost:3306/ingredients";
-        String user= "root";
-        String password= "Eleonora1992Riccardo1988ZeldaMalu!";
+        String url = "jdbc:mysql://localhost:3306/ingredienti";
+        String user = "root";
+        String password = "Eleonora1992Riccardo1988ZeldaMalu!";
 
         try {
-            Connection connection=DriverManager.getConnection(url,user,password);
-            Statement statement=connection.createStatement();
-            String createTableSQL=
-                    "CREATE TABLE Ingredient (" +
-                            "IngredientId INT PRIMARY KEY," +
-                            "IngredientName VARCHAR(255)," +
-                            "Category VARCHAR(50)," +
-                            "ExpirationDate DATE," +
-                            "Quantity INT," +
-                            "UnitOfMeasure VARCHAR(20));";
+            Connection connection = DriverManager.getConnection(url, user, password);
+            Statement statement = connection.createStatement();
+            String createTableSQL =
+                    "CREATE TABLE Food (" +
+                            "NAME VARCHAR(255)," +
+                            "PRICE DOUBLE," +
+                            "CALORIES INT);";
             statement.execute(createTableSQL);
-            System.out.println("Tabella Ingredient Creata.");
-        }catch (SQLException e){
+            System.out.println("Tabella Food Creata.");
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
